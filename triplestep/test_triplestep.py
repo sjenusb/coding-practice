6ea09e0c8c71a03c75e003bfe73212bb7f32a140
Sean Jenkinson <sean.jenkinson@terumobct.com>
2025-02-13 11:51:05 -0700

    Update test_triplestep.py to test non-recursive solution

--- triplestep/test_triplestep.py
+++ triplestep/test_triplestep.py
@@ -5,3 +5,13 @@ def test_triplestep():
    assert triplestep(1) == 1
    assert triplestep(2) == 2
    assert triplestep(3) == 4
+
+def test_triplestep_tribonacci():  
+   assert triplestep_tribonacci(0) == 0
+   assert triplestep_tribonacci(1) == 1
+   assert triplestep_tribonacci(2) == 2
+   assert triplestep_tribonacci(3) == 4
+   assert triplestep_tribonacci(4) == 8
+   assert triplestep_tribonacci(9) == 177
+   assert triplestep_tribonacci(11) == 600
+   assert triplestep_tribonacci(17) == 23249
